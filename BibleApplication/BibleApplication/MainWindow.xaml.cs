16990900e77d37f66bcf0e36f65467d7eb81272c
Adams Bala <adams.alok@gmail.com>
2019-01-22 23:44:25 -0500

    Search button now returns a json object and sets the texts.

--- BibleApplication/BibleApplication/MainWindow.xaml.cs
+++ BibleApplication/BibleApplication/MainWindow.xaml.cs
@@ -1,7 +1,12 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.IO;
 using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -68,10 +73,54 @@ namespace BibleApplication
             }
         }
 
-
+        /// <summary>
+        /// Sets the textblocks with the requested verses.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void btnSearch_Click(object sender, RoutedEventArgs e)
         {
+            // Test URL.
+            // TODO: Get the book version, book name, chapter, and verse and then make the call.
+            var URL = @"http://profo.pythonanywhere.com/bible/api/v1.0/KJV/Ex/40/2";
 
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(URL);
+            try
+            {
+                WebResponse response = request.GetResponse();
+                using (Stream responseStream = response.GetResponseStream())
+                {
+                    StreamReader reader = new StreamReader(responseStream, System.Text.Encoding.UTF8);
+                    var test = reader.ReadToEnd();
+                    var result = JsonConvert.DeserializeObject<Verse>(test);
+                    setTextBoxValue(result.prev.text, result.curr.text, result.next.text);
+                }
+            }
+            catch (WebException ex)
+            {
+                WebResponse errorResponse = ex.Response;
+                using (Stream responseStream = errorResponse.GetResponseStream())
+                {
+                    StreamReader reader = new StreamReader(responseStream, System.Text.Encoding.GetEncoding("utf-8"));
+                    String errorText = reader.ReadToEnd();
+                    // log errorText
+                }
+                throw;
+            }
+        }
+        /// <summary>
+        /// Sets the text of each of the textblocks.
+        /// </summary>
+        /// <param name="prev">Previous text box.</param>
+        /// <param name="curr">Current text box.</param>
+        /// <param name="next">Next text box.</param>
+        private void setTextBoxValue(string prevText, string currText, string nextText)
+        {
+            //TODO: null case for prev and next.
+            prev.Text = prevText;
+            curr.Text = currText;
+            next.Text = nextText;
+     
         }
 
         private void cbBook_Loaded(object sender, RoutedEventArgs e)
@@ -103,5 +152,18 @@ namespace BibleApplication
         #endregion
 
       
+    }
+    public class Verse
+    {
+        public VerseDetails prev { get; set; }
+        public VerseDetails curr { get; set; }
+        public VerseDetails next { get; set; }
+    }
+    public class VerseDetails
+    {
+        public string book { get; set; }
+        public string chapter { get; set; }
+        public string verse { get; set; }
+        public string text { get; set; }
     }
 }
